e64a3e5b37d39f71a62dcfc28b0702ac8435a4cb
Kauane Lourenço <108008606+kauanelourenco@users.noreply.github.com>
2024-04-22 09:16:34 -0300

    1

--- main.js
+++ main.js
@@ -21,4 +21,41 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 const tempos =
 [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-function calculaTempo(tempoObjetivo)
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal =  tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos%= 60;
+    horas%= 24;
+    if (tempoFinal > 0){
+        return[dias,horas,minutos,segundos];
+    } else{
+        return [0,0,0,0];
+    }
+}
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        document.getElementByld("dias"+i).textContent =
+        calculaTempo(tempos[i])[0];
+        document.getElementByld("horas"+i).textContent =
+        calculaTempo(tempos[i])[1];
+        document.getElementByld("min"+i).textContent = 
+        calculaTempo(tempos[i])[2];
+        document.getElementByld("seg"+i).textContent =
+        calculaTempo(tempos[i]) [3];
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro(
+        setInterval(atualizaCronometro,1000);
+    
+}
+
+comecaCronometro()
